e9ac5b976a96078282e1ccd926c5736c90866286
Matthew Darby <me@mjdarby.net>
2015-11-29 04:43:36 +0000

    Make large files way, way less slow. Except on line number change.
    I shouldn't do this recursively.

--- main.py
+++ main.py
@@ -49,6 +49,9 @@ class Application(Frame):
         Frame.__init__(self, master)
         self.pack()
         self.buildWidgets()
+        self.old_line_count = 0
+        self.text_data = {}
+        self.tag_data = {}
 
     def wipe_tags(self, text_entry):
         text_entry.tag_delete("scene_heading")
@@ -57,6 +60,16 @@ class Application(Frame):
         text_entry.tag_delete("parenthetical")
         text_entry.tag_delete("dialogue")
         text_entry.tag_delete("action")
+
+    def wipe_tags_line(self, text_entry, line):
+        start = str(line) + ".0"
+        end = str(line) + ".end"
+        text_entry.tag_remove("scene_heading", start, end)
+        text_entry.tag_remove("transition", start, end)
+        text_entry.tag_remove("character", start, end)
+        text_entry.tag_remove("parenthetical", start, end)
+        text_entry.tag_remove("dialogue", start, end)
+        text_entry.tag_remove("action", start, end)
 
     def format_transitions(self, text_entry):
         start = "1.0"
@@ -74,7 +87,7 @@ class Application(Frame):
     def format_scene_headings(self, text_entry):
         start = "1.0"
         while 1:
-            pos = text_entry.search(r'^(\..+|[Ii][Nn][Tt]|[Ee][Xx][Tt]|[Ee][Ss][Tt]|[Ii][Nn][Tt]\.?/[Ee][Xx][Tt]|[Ii]/[Ee]).+$', start, regexp=True, stopindex=END)
+            pos = text_entry.search(r'^(\..+|[Ii][Nn][Tt]|[Ee][Xx][Tt]|[Ee][Ss][Tt]|[Ii][Nn][Tt]\.?/[Ee][Xx][Tt]|[Ii]/[Ee])[\. ].+$', start, regexp=True, stopindex=END)
             if not pos:
                 break
             line_no = pos[0:pos.index(".")]
@@ -102,6 +115,8 @@ class Application(Frame):
 
     def format_dialogue_and_parentheticals(self, text_entry):
         start = "2.0"
+        end_line_no = text_entry.index(END)
+        end_line_no = int(end_line_no[0:end_line_no.index(".")])
         while 1:
             # Check previous line was dialogue, parenthetical or character
             this_line_no = int(start[0:start.index(".")])
@@ -119,28 +134,29 @@ class Application(Frame):
                 else:
                     text_entry.tag_add("dialogue", my_start, my_end)
 
-            start = text_entry.index(start + " lineend +1c")
-            next_line_no = int(start[0:start.index(".")])
-            last_line_no = text_entry.index(END)
-            last_line_no = int(last_line_no[0:last_line_no.index(".")])
-            if next_line_no >= last_line_no:
+            start = str(int(this_line_no) + 1) + ".0"
+            next_line_no = int(this_line_no) + 1
+            if next_line_no >= end_line_no:
                 break
 
     def format_the_rest(self, text_entry):
         start = "1.0"
+        end_line_no = text_entry.index(END)
+        end_line_no = int(end_line_no[0:end_line_no.index(".")])
         while 1:
             this_line_no = str(int(start[0:start.index(".")]))
             my_start = this_line_no+".0"
             my_end = this_line_no+".end"
-            candidate = not text_entry.tag_names(start)
-            if (candidate):
-                text_entry.tag_add("action", my_start, my_end)
-            start = text_entry.index(start + " lineend +1c")
-            next_line_no = int(start[0:start.index(".")])
-            last_line_no = text_entry.index(END)
-            last_line_no = int(last_line_no[0:last_line_no.index(".")])
-            if next_line_no >= last_line_no:
+            text=text_entry.get(my_start, my_end)
+            tags=text_entry.tag_names(my_start) if text != "" else []
+            self.text_data[int(this_line_no)] = text
+            self.tag_data[int(this_line_no)] = tags
+
+            next_line_no = int(this_line_no) + 1
+            if next_line_no >= end_line_no:
                 break
+            start = str(next_line_no) + ".0"
+
         text_entry.tag_add("action", "1.0", END)
 
     def my_tag_lower(self, text_entry, tag):
@@ -153,6 +169,7 @@ class Application(Frame):
         # Scene first, then character, then dialogue, then parentheses
         self.my_tag_lower(text_entry, "scene_heading")
         self.my_tag_lower(text_entry, "character")
+        self.my_tag_lower(text_entry, "dialogue")
         self.my_tag_lower(text_entry, "action")
 
     def configure_tags(self, text_entry):
@@ -163,18 +180,139 @@ class Application(Frame):
         text_entry.tag_configure("transition", font=(thefont, 12, "bold"), lmargin1="6i", lmargin2="6i", rmargin="1i")
         text_entry.tag_configure("action", font=(thefont, 12), lmargin1="1.5i", lmargin2="1.5i", rmargin="1i")
 
+    def format_line(self, text_entry, line, already_called_previous=False, already_called_next=False):
+        line_no = int(line[0:line.index(".")])
+        start = str(line_no) +".0"
+        end =  str(line_no) + ".end"
+        current_tags = self.tag_data.get(line_no, [])
+        current_text = self.text_data.get(line_no, "")
+        self.wipe_tags_line(text_entry, str(line_no))
+
+        self.format_transitions_line(text_entry, line)
+        self.format_scene_headings_line(text_entry, line)
+        self.format_characters_line(text_entry, line)
+        self.format_dialogue_and_parentheticals_line(text_entry, line)
+
+        text_entry.tag_add("action", start, end)
+
+        self.order_tags(text_entry)
+
+        self.tag_data[line_no] = [x for x in text_entry.tag_names(start)] if text_entry.get(start, end) != "" else []
+        self.text_data[line_no] = text_entry.get(start, end)
+        print(str(line_no))
+        print(self.text_data[line_no])
+        print(current_text)
+        print(self.tag_data[line_no])
+        print(current_tags)
+
+        last_line_no = text_entry.index(END)
+        last_line_no = int(last_line_no[0:last_line_no.index(".")])
+
+        # Check if we needed to change the tags on this line. If so, check for next and previous lines too.
+        # Also need to check on new-lines, which won't change tags.
+        if current_tags != self.tag_data[line_no] or self.text_data[line_no] != current_text:
+            if (line_no > 1):
+                self.format_line(text_entry, str(line_no-1) + ".0", already_called_previous=True)
+            if (line_no + 1 < last_line_no):
+                self.format_line(text_entry, str(line_no+1) + ".0", already_called_next=True)
+
+
+    def format_transitions_line(self, text_entry, line):
+        line_no = int(line[0:line.index(".")])
+        start = str(line_no) +".0"
+        end =  str(line_no) + ".end"
+        pos = text_entry.search(r'^[A-Z ]+:$', start, regexp=True, stopindex=end)
+        if not pos:
+            return
+        if not text_entry.tag_names(index=start):
+            text_entry.tag_add("transition", start, end)
+
+    def format_scene_headings_line(self, text_entry, line):
+        line_no = int(line[0:line.index(".")])
+        start = str(line_no) +".0"
+        end =  str(line_no) + ".end"
+        pos = text_entry.search(r'^(\..+|[Ii][Nn][Tt]|[Ee][Xx][Tt]|[Ee][Ss][Tt]|[Ii][Nn][Tt]\.?/[Ee][Xx][Tt]|[Ii]/[Ee])[\. ].+$', start, regexp=True, stopindex=end)
+        if not pos:
+            return
+        if not text_entry.tag_names(index=start):
+            text_entry.tag_add("scene_heading", start, end)
+
+    def character_candidate(self, text_entry, line):
+        line_no = int(line[0:line.index(".")])
+        start = str(line_no) +".0"
+        end =  str(line_no) + ".end"
+        pos = text_entry.search(r'^(@.*|[A-Z \(\)\'\.,]+)$', start, regexp=True, stopindex=end)
+        return pos
+
+    def format_characters_line(self, text_entry, line):
+        line_no = int(line[0:line.index(".")])
+        start = str(line_no) +".0"
+        end =  str(line_no) + ".end"
+        pos = text_entry.search(r'^(@.*|[A-Z \(\)\'\.,]+)$', start, regexp=True, stopindex=end)
+        if not pos:
+            return
+        next_start = str(line_no+1)+".0"
+        next_end = str(line_no+1)+".end"
+        # Only a character if the next line is non-empty
+        if not text_entry.get(next_start, next_end) == "":
+            # and not text_entry.tag_names(index=start) and not text_entry.tag_names(index=next_start):
+            text_entry.tag_add("character", start, end)
+
+    def format_dialogue_and_parentheticals_line(self, text_entry, line):
+        line_no = int(line[0:line.index(".")])
+        start = str(line_no) +".0"
+        end =  str(line_no) + ".end"
+        last_line_no = text_entry.index(END)
+        last_line_no = int(last_line_no[0:last_line_no.index(".")])
+        # Check previous line was dialogue, parenthetical or character
+        previous_line_no = str(line_no - 1)
+        previous_line_start = previous_line_no + ".0"
+        previous_line_end = previous_line_no + ".end"
+        previous_line_tags = text_entry.tag_names(previous_line_no+".0") if text_entry.get(previous_line_start, previous_line_end) != "" else []
+        print(str(previous_line_no))
+        print(previous_line_tags)
+        candidate = ("character" in previous_line_tags or
+                     "parenthetical" in previous_line_tags or
+                     "dialogue" in previous_line_tags)
+        if (candidate):
+            parenthetical = text_entry.search(r'^\(', start, regexp=True, stopindex=end)
+            if parenthetical:
+                text_entry.tag_add("parenthetical", start, end)
+            else:
+                text_entry.tag_add("dialogue", start, end)
+
+    def process_text_new(self, event):
+        text_entry = self.containing_frame.writing_frame.text_entry
+        new_line_count = text_entry.index(END)
+        new_line_count = int(new_line_count[0:new_line_count.index(".")])
+
+        # Try and guess how many lines were modified. If it's a lot, process whole file.
+        # Else just the modified line and anything branching off that
+        if abs(new_line_count - self.old_line_count):
+            self.process_text(event)
+        else:
+            current_line_start = text_entry.index(INSERT + " linestart")
+            next_line_start = text_entry.index(INSERT + " lineend")
+            next_line_start = text_entry.index(next_line_start + "+1c")
+            previous_line_start = text_entry.index(current_line_start + "-1c linestart")
+            self.format_line(text_entry, current_line_start)
+            self.configure_tags(text_entry)
+
+        self.old_line_count = new_line_count
+
     def process_text(self, event):
         text_entry = self.containing_frame.writing_frame.text_entry
 
         self.wipe_tags(text_entry)
-        self.configure_tags(text_entry)
 
         self.format_transitions(text_entry)
         self.format_scene_headings(text_entry)
         self.format_characters(text_entry)
         self.format_dialogue_and_parentheticals(text_entry)
         self.format_the_rest(text_entry)
+
         self.order_tags(text_entry)
+        self.configure_tags(text_entry)
 
     def tag_to_left_margin(self, tag):
         if tag == "scene_heading":
@@ -213,6 +351,12 @@ class Application(Frame):
 
 
     def pdf(self):
+        def remove_leading_special_characters(text):
+            new_text = text
+            if text and text[0] in [".", "!", "@", "~", ">"]:
+                new_text = text[1:]
+            return new_text
+
         text_entry = self.containing_frame.writing_frame.text_entry
 
         pdf = fpdf.FPDF('P', 'in', format='letter')
@@ -239,6 +383,7 @@ class Application(Frame):
                     if (pdf.y + 0.17 * 2 > pdf.page_break_trigger):
                         pdf.multi_cell(width, 0.17*2, txt="", align=align)
             text=text_entry.get(start, this_line_no+".end")
+            text=remove_leading_special_characters(text)
             pdf.set_x(left_margin)
             pdf.multi_cell(width, 0.17, txt=text, align=align)
             start = text_entry.index(start + " lineend +1c")
@@ -267,7 +412,7 @@ class Application(Frame):
         self.containing_frame.writing_frame = WritingFrame(self.containing_frame)
 
         text_entry = self.containing_frame.writing_frame.text_entry
-        text_entry.beenModified = self.process_text
+        text_entry.beenModified = self.process_text_new
         self.configure_tags(text_entry)
         text_entry.tag_add("action", "1.0", END)
 
@@ -285,6 +430,7 @@ class Application(Frame):
         self.pdf.pack(side="bottom")
 
 
+import cProfile as profile
 if __name__ == "__main__":
     sys.stdout = open("output.log", "w")
     sys.stderr = open("errors.log", "w")
@@ -294,4 +440,5 @@ if __name__ == "__main__":
     app = Application(master=root)
     app.master.title("Scripter")
     app.master.minsize(400, 400)
+#    profile.run('app.mainloop()')
     app.mainloop()
